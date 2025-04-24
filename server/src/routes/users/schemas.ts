d2023f81775d686cf769f30f734e01dc6266a1c3
Daniel Vilela <danielvilelayo@gmail.com>
2025-04-23 22:32:06 -0500

    feat: add users routes

---
+++ server/src/routes/users/schemas.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+const USER_ROLES = ["admin", "customer"] as const;
+
+export const userModel = z.object({
+  name: z.string().nonempty(),
+  id: z.string().nonempty(),
+  email: z.optional(z.string()),
+  phone: z.optional(
+    z.object({
+      country_code: z.optional(z.number()).default(51),
+      number: z.number(),
+    }),
+  ),
+  role: z.enum(USER_ROLES),
+});
+
+export type UserModel = z.infer<typeof userModel>;
